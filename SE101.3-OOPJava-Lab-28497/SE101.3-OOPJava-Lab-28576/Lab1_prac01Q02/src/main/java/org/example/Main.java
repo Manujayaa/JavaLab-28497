690c5bbbe9ce3579381462820a8187e56e0e558f
Manujayaa <136641784+Manujayaa@users.noreply.github.com>
2023-07-31 17:06:47 +0530

    Add files via upload

---
+++ SE101.3-OOPJava-Lab-28497/SE101.3-OOPJava-Lab-28576/Lab1_prac01Q02/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.mavenproject12;
+public class Mavenproject12 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Manujaya Rathnayake");
+        System.out.println("Management infomation system");
+    }
+}
